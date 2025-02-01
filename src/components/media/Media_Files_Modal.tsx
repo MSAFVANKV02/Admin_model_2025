296cf5562099a778f1eb04210f0c0e9bcd21b1c4
haashtech <haashtechnologies@gmail.com>
2025-02-01 13:35:38 +0530

    completed selecting images from media drawer in product page

--- src/components/media/Media_Files_Modal.tsx
+++ src/components/media/Media_Files_Modal.tsx
@@ -1,20 +1,99 @@
-import AllUploadedFiles from "@/pages/settings/media/retrive/all_uploaded_files";
-import TaskModal, {
-  TaskModalContent,
-  TaskModalFooter,
-  TaskModalHeader,
-} from "../modals/TaskModal";
+// import AllUploadedFiles from "@/pages/settings/media/retrive/all_uploaded_files";
+// import TaskModal, {
+//   TaskModalContent,
+//   TaskModalFooter,
+//   TaskModalHeader,
+// } from "../modals/TaskModal";
+// import AyButton from "../myUi/AyButton";
+// import { memo, useState } from "react";
+// import { useModal } from "@/providers/context/context";
+// import MyCloseIcon from "../icons/My_CloseIcon";
+
+// type Props = {
+//   setFieldValues?: (name: string, value: any) => void;
+//   fieldName?: string;
+//   multiple?: boolean;
+//   handleFileUpload: (event: string[], fieldName: string) => void;
+//   mediaType?: "pdf" | "image" ;
+// };
+
+// function MediaFilesModal({
+//   fieldName,
+//   multiple,
+//   handleFileUpload,
+//   mediaType,
+// }: Props) {
+//   const [selectedFiles, setSelectedFiles] = useState<string[]>([]);
+//   const { setIsOpen } = useModal();
+
+//   const handleFileSelection = (src: string[]) => {
+//     if (multiple) {
+//       const newSelectedFiles = src;
+//       setSelectedFiles(newSelectedFiles);
+//     } else {
+//       const [selectedFile] = src;
+//       setSelectedFiles([selectedFile]);
+//     }
+//   };
+
+//   const handleSelectFiles = () => {
+//     if (fieldName) {
+//       handleFileUpload(selectedFiles, fieldName);
+//       setIsOpen(false);
+//     }
+//   };
+
+//   // console.log(fieldName, "fieldName in nowhere");
+
+//   return (
+//     <TaskModal className="md:w-[70%] md:h-[90%]">
+//       <TaskModalHeader className="justify-end bg-gray-50 rounded-md">
+//         {/* <h1 className="text-textGray font-bold">Select Files</h1> */}
+//         <MyCloseIcon 
+//         onClick={()=>{
+//           setIsOpen(false);
+//         }}
+//         />
+//       </TaskModalHeader>
+
+//       <TaskModalContent className="max-h-[90%] scrollbar-none overflow-y-scroll">
+//         <AllUploadedFiles
+//           onClick={(src) => handleFileSelection(src)}
+//           multiple={multiple} // Enable multiple file selection
+//           mediaType={mediaType} // Show only images
+//         />
+//       </TaskModalContent>
+//       <TaskModalFooter>
+//         <AyButton title="Select" type="button" onClick={handleSelectFiles} />
+//       </TaskModalFooter>
+//     </TaskModal>
+//   );
+// }
+
+// export default memo(MediaFilesModal) 
+// =================================================================
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+
 import AyButton from "../myUi/AyButton";
 import { memo, useState } from "react";
 import { useModal } from "@/providers/context/context";
 import MyCloseIcon from "../icons/My_CloseIcon";
+import AllUploadedFiles, { IFileDataMedia } from "@/pages/settings/media/retrive/all_uploaded_files";
+
 
 type Props = {
   setFieldValues?: (name: string, value: any) => void;
   fieldName?: string;
   multiple?: boolean;
-  handleFileUpload: (event: string[], fieldName: string) => void;
-  mediaType?: "pdf" | "image" ;
+  handleFileUpload?: (event: IFileDataMedia[], fieldName: string) => void;
+  mediaType?: "pdf" | "image" | "videos" | "xl" | "";
 };
 
 function MediaFilesModal({
@@ -23,10 +102,10 @@ function MediaFilesModal({
   handleFileUpload,
   mediaType,
 }: Props) {
-  const [selectedFiles, setSelectedFiles] = useState<string[]>([]);
-  const { setIsOpen } = useModal();
+  const [selectedFiles, setSelectedFiles] = useState<IFileDataMedia[]>([]);
+  const { setMediaOpenDrawer, mediaOpenDrawer } = useModal();
 
-  const handleFileSelection = (src: string[]) => {
+  const handleFileSelection = (src: IFileDataMedia[]) => {
     if (multiple) {
       const newSelectedFiles = src;
       setSelectedFiles(newSelectedFiles);
@@ -37,37 +116,60 @@ function MediaFilesModal({
   };
 
   const handleSelectFiles = () => {
-    if (fieldName) {
+    if (fieldName && handleFileUpload) {
       handleFileUpload(selectedFiles, fieldName);
-      setIsOpen(false);
+      setMediaOpenDrawer(false);
     }
   };
 
   // console.log(fieldName, "fieldName in nowhere");
 
   return (
-    <TaskModal className="md:w-[70%] md:h-[90%]">
-      <TaskModalHeader className="justify-end bg-gray-50 rounded-md">
-        {/* <h1 className="text-textGray font-bold">Select Files</h1> */}
-        <MyCloseIcon 
-        onClick={()=>{
-          setIsOpen(false);
-        }}
-        />
-      </TaskModalHeader>
+    <Drawer
+      open={mediaOpenDrawer}
+      onOpenChange={(open) => setMediaOpenDrawer(open)}
+    >
+      {/* <DrawerTrigger>Open</DrawerTrigger> */}
+      <div className="mx-auto w-full max-w-sm ">
+        <DrawerContent className="z-[20001] mx-auto max-w-screen-xl w-full h-[85vh] max-h-[85vh] flex flex-col rounded-lg border bg-background p-4">
+          <DrawerHeader className="flex-shrink-0">
+            <DrawerTitle className="flex justify-end">
+              <MyCloseIcon
+                onClick={() => {
+                  setMediaOpenDrawer(false);
+                }}
+              />
+            </DrawerTitle>
+          </DrawerHeader>
 
-      <TaskModalContent className="max-h-[90%] scrollbar-none overflow-y-scroll">
-        <AllUploadedFiles
-          onClick={(src) => handleFileSelection(src)}
-          multiple={multiple} // Enable multiple file selection
-          mediaType={mediaType} // Show only images
-        />
-      </TaskModalContent>
-      <TaskModalFooter>
-        <AyButton title="Select" type="button" onClick={handleSelectFiles} />
-      </TaskModalFooter>
-    </TaskModal>
+          {/* Content Scrollable */}
+          <div className="flex-grow overflow-y-auto">
+            <AllUploadedFiles
+              onClick={(selectedFiles) => {
+                // console.log(selectedFiles,'selectedFiles');
+                // console.log(src,'src');
+
+                handleFileSelection(selectedFiles);
+              }}
+              multiple={multiple}
+              mediaType={mediaType}
+            />
+          </div>
+
+          {/* Footer Stays Visible */}
+          <DrawerFooter className="flex-shrink-0">
+            <DrawerClose>
+              <AyButton
+                title="Select"
+                type="button"
+                onClick={handleSelectFiles}
+              />
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </div>
+    </Drawer>
   );
 }
 
-export default memo(MediaFilesModal) 
+export default memo(MediaFilesModal);
--- src/pages/products/add-new/initialValues.ts
+++ src/pages/products/add-new/initialValues.ts
@@ -1,63 +1,61 @@
 export const InitialValues = {
-    product_name: '',
-    mrp: 0,
-    product_sku: '',
-    barcode: '',
-    brand: '',
-    keywords: '',
-    minimum_quantity: 0,
-    product_weight: 0,
-    product_owner:'',
-    // height: 0,
-    // length: 0,
-    // width:0,
-    product_dimensions: {
-      product_height: 0,
-      product_length: 0,
-      product_width: 0,
-    },
-    // dimensions: '',
-    tax_details:{
-      taxSlab: [],
-      isCess: false,
-      cess: null,
-    },
+  product_name: '',
+  mrp: undefined,
+  product_sku: '',
+  barcode: '',
+  brand: '',
+  keywords: '',
+  minimum_quantity: undefined,
+  product_weight: undefined,
+  product_owner:'',
+  // height: undefined,
+  // length: undefined,
+  // width:undefined,
+  product_dimensions: {
+    product_height: undefined,
+    product_length: undefined,
+    product_width: undefined,
+  },
+  // dimensions: '',
+  tax_details:{
+    taxSlab: [],
+    isCess: false,
+    cess: undefined,
+  },
 // isCess: false,
 //     cess: [],
 //     taxSlab: [],
-    is_published:false,
-    status: "",
-    is_todays_deal: false,
-    is_featured_product:false,
-    description: '',
-    
-    // ===== File upload section =================
-    gallery_image: [],
-    thumbnails:[],
-    // productImages: [],
-    sizeImages: [],
-    // ===== price stock section =================
+  is_published:false,
+  status: "",
+  is_todays_deal: false,
+  is_featured_product:false,
+  description: '',
+  
+  // ===== File upload section =================
+  gallery_image: [],
+  thumbnails:[],
+  // productImages: [],
+  sizeImages: [],
+  // ===== price stock section =================
 
-    base_price:null,
-    sample_price:null,
-    discount:null,
-    discount_type:"",
-    price_per_pieces:[],
-    selectWise:"size",
-    store:"",
-    variations:[],
-    // variations: [
-    //   {
-    //     colorCode: '',
-    //     colorName: '',
-    //     image: null,
-    //     details: [{ size: '', stock: 0, mrp: 0, selling_price: 0 }],
-    //   },
-    // ],
+  base_price:undefined,
+  sample_price:undefined,
+  discount:undefined,
+  discount_type:"",
+  price_per_pieces:[],
+  selectWise:"size",
+  variations:[],
+  // variations: [
+  //   {
+  //     colorCode: '',
+  //     colorName: '',
+  //     image: null,
+  //     details: [{ size: '', stock: undefined, mrp: undefined, selling_price: undefined }],
+  //   },
+  // ],
 
-    // ===== shipping section =================
-    cod:false,
-    freeShipping:false,
-    
-  }
- 
+  // ===== shipping section =================
+  cod:false,
+  freeShipping:false,
+  
+}
